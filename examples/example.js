2a646ef82e5b671f223201e0f9bae70b1c9ed4ce
Alex Shteinikov <alex.shteinikov@gmail.com>
2015-03-31 22:04:35 +1000

    added a few rules

---
+++ examples/example.js
@@ -0,0 +1,40 @@
+var jsonlt = require('../lib/jsonlt');
+
+var src = {
+    "firstName": "John",
+    "lastName": "Smith",
+    "address": {
+        "suburb": "Brisbane",
+        "postCode": 4000,
+        "street": "200 Adelaide"
+    },
+    "orders": [ 2342000, 2342001, 23400015],
+    "items": [
+        {
+            "type": "book",
+            "description": "Simple book..."
+        },
+        {
+            "type": "CD",
+            "description": "Simple music CD",
+            "artist": "Unknown Artist"
+        }
+    ]
+};
+
+var rules = {
+    "@jsonlt": 1,
+    "person": {
+        "firstName": "#firstName",
+        "lastName": "#lastName"
+    },
+    "lastOrder": "#orders[-1]",
+    "ordersString": "#(string)orders",
+    "CDs": "#items|type='CD'",
+    "descriptions": "#items*description",
+    "address": "#address^,",
+    "addressNormal": "#address.street, #address.suburb #address.postCode",
+    "totalOrders": "#orders.length"
+};
+
+console.log(jsonlt.transform(src, rules));
